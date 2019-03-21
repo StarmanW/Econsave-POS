f5f363e96aa1fe17c44fc2b3bc877c165c5f7b81
chongjh <chongjh-sa16@student.tarc.edu.my>
2019-03-21 22:46:52 +0800

    Sales UI

--- Econsave-POS/Modules/Sales/Sales.Designer.vb
+++ Econsave-POS/Modules/Sales/Sales.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Sales
-    Inherits System.Windows.Forms.Form
+    Inherits MetroFramework.Forms.MetroForm
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,11 +20,201 @@ Partial Class Sales
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.MenuStrip2 = New System.Windows.Forms.MenuStrip()
+        Me.SalesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ItemManagementToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AddNewItemToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DisplayItemListToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ProfileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.MetroTextBox1 = New MetroFramework.Controls.MetroTextBox()
+        Me.MetroButton1 = New MetroFramework.Controls.MetroButton()
+        Me.MetroButton2 = New MetroFramework.Controls.MetroButton()
+        Me.Tota = New MetroFramework.Controls.MetroLabel()
+        Me.MetroLabel1 = New MetroFramework.Controls.MetroLabel()
+        Me.MenuStrip2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip2
+        '
+        Me.MenuStrip2.Font = New System.Drawing.Font("Segoe UI", 10.0!)
+        Me.MenuStrip2.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip2.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SalesToolStripMenuItem, Me.ItemManagementToolStripMenuItem, Me.ProfileToolStripMenuItem})
+        Me.MenuStrip2.Location = New System.Drawing.Point(20, 60)
+        Me.MenuStrip2.Name = "MenuStrip2"
+        Me.MenuStrip2.Padding = New System.Windows.Forms.Padding(8, 4, 0, 4)
+        Me.MenuStrip2.Size = New System.Drawing.Size(1072, 35)
+        Me.MenuStrip2.TabIndex = 2
+        Me.MenuStrip2.Text = "MenuStrip2"
+        '
+        'SalesToolStripMenuItem
+        '
+        Me.SalesToolStripMenuItem.Name = "SalesToolStripMenuItem"
+        Me.SalesToolStripMenuItem.Size = New System.Drawing.Size(60, 27)
+        Me.SalesToolStripMenuItem.Text = "Sales"
+        '
+        'ItemManagementToolStripMenuItem
+        '
+        Me.ItemManagementToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AddNewItemToolStripMenuItem, Me.DisplayItemListToolStripMenuItem})
+        Me.ItemManagementToolStripMenuItem.Name = "ItemManagementToolStripMenuItem"
+        Me.ItemManagementToolStripMenuItem.Size = New System.Drawing.Size(164, 27)
+        Me.ItemManagementToolStripMenuItem.Text = "Item Management"
+        '
+        'AddNewItemToolStripMenuItem
+        '
+        Me.AddNewItemToolStripMenuItem.Name = "AddNewItemToolStripMenuItem"
+        Me.AddNewItemToolStripMenuItem.Size = New System.Drawing.Size(216, 28)
+        Me.AddNewItemToolStripMenuItem.Text = "Add New Item"
+        '
+        'DisplayItemListToolStripMenuItem
+        '
+        Me.DisplayItemListToolStripMenuItem.Name = "DisplayItemListToolStripMenuItem"
+        Me.DisplayItemListToolStripMenuItem.Size = New System.Drawing.Size(216, 28)
+        Me.DisplayItemListToolStripMenuItem.Text = "Display Item List"
+        '
+        'ProfileToolStripMenuItem
+        '
+        Me.ProfileToolStripMenuItem.Name = "ProfileToolStripMenuItem"
+        Me.ProfileToolStripMenuItem.Size = New System.Drawing.Size(70, 27)
+        Me.ProfileToolStripMenuItem.Text = "Profile"
+        '
+        'Panel1
+        '
+        Me.Panel1.AutoScroll = True
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.Panel1.Location = New System.Drawing.Point(20, 155)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(549, 518)
+        Me.Panel1.TabIndex = 3
+        '
+        'Panel2
+        '
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel2.Location = New System.Drawing.Point(600, 155)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(407, 322)
+        Me.Panel2.TabIndex = 4
+        '
+        'MetroTextBox1
+        '
+        '
+        '
+        '
+        Me.MetroTextBox1.CustomButton.Image = Nothing
+        Me.MetroTextBox1.CustomButton.Location = New System.Drawing.Point(195, 1)
+        Me.MetroTextBox1.CustomButton.Name = ""
+        Me.MetroTextBox1.CustomButton.Size = New System.Drawing.Size(33, 33)
+        Me.MetroTextBox1.CustomButton.Style = MetroFramework.MetroColorStyle.Blue
+        Me.MetroTextBox1.CustomButton.TabIndex = 1
+        Me.MetroTextBox1.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light
+        Me.MetroTextBox1.CustomButton.UseSelectable = True
+        Me.MetroTextBox1.CustomButton.Visible = False
+        Me.MetroTextBox1.FontSize = MetroFramework.MetroTextBoxSize.Tall
+        Me.MetroTextBox1.Lines = New String() {"Search..."}
+        Me.MetroTextBox1.Location = New System.Drawing.Point(20, 114)
+        Me.MetroTextBox1.MaxLength = 32767
+        Me.MetroTextBox1.Name = "MetroTextBox1"
+        Me.MetroTextBox1.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.MetroTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.None
+        Me.MetroTextBox1.SelectedText = ""
+        Me.MetroTextBox1.SelectionLength = 0
+        Me.MetroTextBox1.SelectionStart = 0
+        Me.MetroTextBox1.ShortcutsEnabled = True
+        Me.MetroTextBox1.Size = New System.Drawing.Size(229, 35)
+        Me.MetroTextBox1.TabIndex = 5
+        Me.MetroTextBox1.Text = "Search..."
+        Me.MetroTextBox1.UseSelectable = True
+        Me.MetroTextBox1.WaterMarkColor = System.Drawing.Color.FromArgb(CType(CType(109, Byte), Integer), CType(CType(109, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.MetroTextBox1.WaterMarkFont = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        '
+        'MetroButton1
+        '
+        Me.MetroButton1.BackColor = System.Drawing.SystemColors.Highlight
+        Me.MetroButton1.FontSize = MetroFramework.MetroButtonSize.Tall
+        Me.MetroButton1.ForeColor = System.Drawing.Color.White
+        Me.MetroButton1.Location = New System.Drawing.Point(817, 627)
+        Me.MetroButton1.Name = "MetroButton1"
+        Me.MetroButton1.Size = New System.Drawing.Size(114, 46)
+        Me.MetroButton1.TabIndex = 6
+        Me.MetroButton1.Text = "C&lear"
+        Me.MetroButton1.UseCustomBackColor = True
+        Me.MetroButton1.UseCustomForeColor = True
+        Me.MetroButton1.UseSelectable = True
+        '
+        'MetroButton2
+        '
+        Me.MetroButton2.BackColor = System.Drawing.Color.Green
+        Me.MetroButton2.FontSize = MetroFramework.MetroButtonSize.Tall
+        Me.MetroButton2.ForeColor = System.Drawing.Color.White
+        Me.MetroButton2.Location = New System.Drawing.Point(665, 627)
+        Me.MetroButton2.Name = "MetroButton2"
+        Me.MetroButton2.Size = New System.Drawing.Size(114, 46)
+        Me.MetroButton2.TabIndex = 7
+        Me.MetroButton2.Text = "&Submit"
+        Me.MetroButton2.UseCustomBackColor = True
+        Me.MetroButton2.UseCustomForeColor = True
+        Me.MetroButton2.UseSelectable = True
+        '
+        'Tota
+        '
+        Me.Tota.FontSize = MetroFramework.MetroLabelSize.Tall
+        Me.Tota.FontWeight = MetroFramework.MetroLabelWeight.Regular
+        Me.Tota.Location = New System.Drawing.Point(653, 554)
+        Me.Tota.Name = "Tota"
+        Me.Tota.Size = New System.Drawing.Size(114, 30)
+        Me.Tota.TabIndex = 8
+        Me.Tota.Text = "Total : "
+        Me.Tota.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'MetroLabel1
+        '
+        Me.MetroLabel1.FontSize = MetroFramework.MetroLabelSize.Tall
+        Me.MetroLabel1.FontWeight = MetroFramework.MetroLabelWeight.Regular
+        Me.MetroLabel1.Location = New System.Drawing.Point(769, 554)
+        Me.MetroLabel1.Name = "MetroLabel1"
+        Me.MetroLabel1.Size = New System.Drawing.Size(150, 30)
+        Me.MetroLabel1.TabIndex = 9
+        Me.MetroLabel1.Text = "RM 29.50"
+        Me.MetroLabel1.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'Sales
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Staff"
+        Me.ClientSize = New System.Drawing.Size(1112, 756)
+        Me.Controls.Add(Me.MetroLabel1)
+        Me.Controls.Add(Me.Tota)
+        Me.Controls.Add(Me.MetroButton2)
+        Me.Controls.Add(Me.MetroButton1)
+        Me.Controls.Add(Me.MetroTextBox1)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.MenuStrip2)
+        Me.Name = "Sales"
+        Me.Text = "Sales"
+        Me.Theme = MetroFramework.MetroThemeStyle.[Default]
+        Me.MenuStrip2.ResumeLayout(False)
+        Me.MenuStrip2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents MenuStrip2 As MenuStrip
+    Friend WithEvents SalesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ItemManagementToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AddNewItemToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DisplayItemListToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ProfileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents MetroTextBox1 As MetroFramework.Controls.MetroTextBox
+    Friend WithEvents MetroButton1 As MetroFramework.Controls.MetroButton
+    Friend WithEvents MetroButton2 As MetroFramework.Controls.MetroButton
+    Friend WithEvents Tota As MetroFramework.Controls.MetroLabel
+    Friend WithEvents MetroLabel1 As MetroFramework.Controls.MetroLabel
 End Class
--- Econsave-POS/Modules/Sales/Sales.vb
+++ Econsave-POS/Modules/Sales/Sales.vb
@@ -1,3 +1,20 @@
 ﻿Public Class Sales
 
+
+    Private Sub Sales_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'testing purpose - delete later
+        Dim DynamicButtonCount As Integer = 1
+
+        For i = 0 To 50
+            Dim name As String = ("Dynamic Button_" & DynamicButtonCount)
+            Dim btnDynamicButton As Button = New Button
+            btnDynamicButton.Name = name
+            btnDynamicButton.Text = name
+            btnDynamicButton.Size = New System.Drawing.Size(200, 30)
+            btnDynamicButton.Location = New System.Drawing.Point(40, (DynamicButtonCount * 40))
+            Panel1.Controls.Add(btnDynamicButton)
+            DynamicButtonCount = (DynamicButtonCount + 1)
+        Next
+    End Sub
+
 End Class
