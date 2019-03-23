1d65397f1267d042cfdad3f0688c6d716108105b
StarmanW <swkf3011@hotmail.com>
2019-03-23 17:01:35 +0800

    Revert "Revert "Merge branch 'master' of https://github.com/StarmanW/Econsave-POS""
    This reverts commit 93d29d065e04a838a9269dc2d4a563ecc50aecdb.

---
+++ Econsave-POS/Modules/StaffManagement/RetrieveStaff.vb
@@ -0,0 +1,7 @@
+﻿Public Class RetrieveStaff
+    Private Sub RetrieveStaff_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'EconsaveDBDataSet.Staff' table. You can move, or remove it, as needed.
+        Me.StaffTableAdapter.Fill(Me.EconsaveDBDataSet.Staff)
+
+    End Sub
+End Class
